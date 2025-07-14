7e3ffd32499809d73862f082a09f91453a9adf95
jtranberg <jtranberg@hotmail.ca>
2025-07-14 12:30:20 -0700

    logo and theme

--- server.js
+++ server.js
@@ -23,9 +23,16 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
+
 
 app.use(express.json());
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
