64274f1af3308d1b0e75552cc3e3cef3e9438148
jtranberg <jtranberg@hotmail.ca>
2026-03-01 22:24:16 -0800

    Initial TS syndicator standalone

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import Document from "./models/Document.js";
 import Tenant from "./models/Tenant.js";
 import Property from "./models/Property.js";
 import Note from "./models/Note.js";
-import webflowPropertiesRoutes from "./src/routes/webflowpropertiesroutes.ts/index.js";
+import webflowPropertiesRoutes from "./src/routes/webflowpropertiesroutes.ts";
 
 dotenv.config();
 
