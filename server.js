fdee4261718600bf4d0d437dda62e967e84e222f
jtranberg <jtranberg@hotmail.ca>
2026-03-04 16:55:27 -0800

    emails

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use(
 app.options(/.*/, cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
-app.use("/api", importProxyRoutes);
+app.use("/api/webflow", importProxyRoutes);
 
 
 // MongoDB Connection...
